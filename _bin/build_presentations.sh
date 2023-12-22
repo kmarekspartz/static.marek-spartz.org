2321970f94f576f4e41b621b8ebbacda808e6946
Kyle Marek-Spartz <kyle.marek.spartz@gmail.com>
2023-12-22 17:32:03 -0600

    Add compiled presentations

--- _bin/build_presentations.sh
+++ _bin/build_presentations.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-for f in $(find presentations -name "*.md"); do
+for f in $(find kyle/presentations -name "*.md"); do
     new_f=$(echo "$f" | sed s/.md$/.html/)
     echo "Building $f into $new_f"
     pandoc -s --webtex -i -t slidy $f -o $new_f
